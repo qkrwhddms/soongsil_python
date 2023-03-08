35b241df7251f5ee360e3b61ad677b91eb0f51a7
qkrwhddms <qkrwhddms01@gmail.com>
2023-03-09 02:15:39 +0900

    터틀_거북이 모양

---
+++ soongsil/myturtle.py
@@ -0,0 +1,13 @@
+import turtle
+
+# 거북이 모양으로 터틀 객체 생성
+t = turtle.Turtle()
+t.shape("turtle")
+
+# 삼각형 그리기
+for i in range(3):
+    t.forward(100)   # 선 그리기
+    t.left(120)      # 120도 왼쪽으로 회전
+
+# 그리기 종료
+turtle.done()
