888910760b9accbb22b76d2e162f22721693397c
qkrwhddms <qkrwhddms01@gmail.com>
2023-03-22 23:05:35 +0900

    변수와 자료형

---
+++ soongsil/cricle_area.py
@@ -0,0 +1,12 @@
+#변수와 자료형
+
+# 변수 redius에 값을 저장한다.
+radius = 10     # radius는 이제 10이다.
+
+# 면적을 계산한다.
+area1 = 3.14 * radius * radius
+area2 = 3.14 * radius ** 2
+
+# 결과를 출력한다.
+print("1반지름", radius, "인 원의 면적 = ", area1)
+print("2반지름", radius, "인 원의 면적 = ", area2)
