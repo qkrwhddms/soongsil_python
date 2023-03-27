35d9dfcfbd576724db7e05bb32c1da21b09ff747
qkrwhddms <qkrwhddms01@gmail.com>
2023-03-27 22:45:30 +0900

    조건연산자

---
+++ soongsil/average(p.93).py
@@ -0,0 +1,7 @@
+# 수식과 연산자
+score1 = float(input("국어 성적: "))
+score2 = float(input("수학 성적: "))
+score3 = float(input("영어 성적: "))
+
+avg = (score1 + score2 + score3) / 3.0
+print(f"\n평균 성적은 {avg:.01f}점 입니다.")
