d19173e041128d7b056219a685b66ce53a5bc99d
qkrwhddms <qkrwhddms01@gmail.com>
2023-04-04 22:28:08 +0900

    반복문 Turtle n각형 그리기

---
+++ soongsil/turtle.py
@@ -0,0 +1,15 @@
+# 반복문
+# n각형 그리기
+
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+
+s = turtle.textinput("", "몇각형을 원하시나요?:")
+n = int(s)
+
+for i in range(n):
+    t.forward(100)
+    t.left(360/n)
+
+turtle.done()
