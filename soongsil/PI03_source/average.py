32835e974ee46a2ee445d6d5984751de5ccc38a0
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2023-06-16 20:23:39 +0900

    Create average.py

---
+++ soongsil/PI03_source/average.py
@@ -0,0 +1,6 @@
+score1 = float(input("국어 성적: "))
+score2 = float(input("수학 성적: "))
+score3 = float(input("영어 성적: "))
+
+avg = ( score1 + score2 + score3 ) / 3.0  
+print(f"\n평균 성적은 {avg:.01f}점입니다.") 
